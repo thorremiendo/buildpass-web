6a8e0aa2fc31f2aa264421784a65c7f9f132fd36
Thor Remiendo <thorremiendo@gmail.com>
2020-12-07 09:23:26 +0800

    added table forms for evaluators

--- src/app/evaluator-dashboard/cbao-evaluator/cbao-evaluator.component.ts
+++ src/app/evaluator-dashboard/cbao-evaluator/cbao-evaluator.component.ts
@@ -7,24 +7,51 @@ import {
 import { FormDetailsComponent } from '../form-details/form-details.component';
 
 const FORM_TABLE_DATA = [
-  { name: 'Zoning Clearance Application Form', status: 'For Checking' },
-  { name: 'Building Permit Form', status: 'For Checking' },
-  { name: 'Sanitary Permit Form', status: 'For Checking' },
-  { name: 'Electrical Permit Form', status: 'For Checking' },
+  {
+    name: 'Zoning Clearance Application Form',
+    status: '--',
+    remarks: '--',
+  },
+  {
+    name: 'Building Permit Form',
+    status: '--',
+    remarks: '--',
+  },
+  {
+    name: 'Sanitary Permit Form',
+    status: '--',
+    remarks: '--',
+  },
+  {
+    name: 'Electrical Permit Form',
+    status: '--',
+    remarks: '--',
+  },
   {
     name: 'Authorization Letter of Special Power of Attorney',
-    status: 'For Checking',
+    status: '--',
+    remarks: '--',
   },
-  { name: 'Filing Fee proof of payment (receipt)', status: 'For Checking' },
+  {
+    name: 'Filing Fee proof of payment (receipt)',
+    status: '--',
+    remarks: '--',
+  },
   {
     name: 'Tax Declaration from City Assessors Office',
-    status: 'For Checking',
+    status: '--',
+    remarks: '--',
   },
   {
     name: 'Latest Quarter of the Real Property Tax Receipt',
-    status: 'For Checking',
+    status: '--',
+    remarks: '--',
   },
-  { name: 'Clear Latest picture of site/are', status: 'For Checking' },
+  {
+    name: 'Clear Latest picture of site/are',
+    status: '--',
+    remarks: '--',
+  },
 ];
 @Component({
   selector: 'app-cbao-evaluator',
@@ -32,7 +59,7 @@ const FORM_TABLE_DATA = [
   styleUrls: ['./cbao-evaluator.component.scss'],
 })
 export class CbaoEvaluatorComponent implements OnInit {
-  displayedColumns: string[] = ['name', 'status', 'action'];
+  displayedColumns: string[] = ['index','name', 'status', 'remarks', 'action'];
   dataSource = FORM_TABLE_DATA;
   public pdfSrc =
     'https://baguio-ocpas.s3-ap-southeast-1.amazonaws.com/forms/Application_Form_for_Certificate_of_Zoning_Compliance-revised_by_TSA-Sept_4__2020+(1).pdf';
@@ -42,7 +69,7 @@ export class CbaoEvaluatorComponent implements OnInit {
   openFormDialog(): void {
     const dialogRef = this.dialog.open(FormDetailsComponent, {
       width: '1500px',
-      height: '2000px'
+      height: '2000px',
     });
 
     dialogRef.afterClosed().subscribe((result) => {
