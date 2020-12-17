a54d3341ef4580c978d0d45d12b126c0fb1d79d0
Evan <nyamet@pm.me>
2020-12-17 13:17:37 +0800

    added faq and ordinance sections to landing page

--- src/app/landing/forgot-password/forgot-password.component.ts
+++ src/app/landing/forgot-password/forgot-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
 
