dbfb39b7a06b6c2ecea2b81307c65a3105b62c69
Evan <nyamet@pm.me>
2021-01-21 13:43:12 +0800

    edit profile

--- src/app/evaluator-dashboard/evaluator-edit-profile/evaluator-edit-profile.component.ts
+++ src/app/evaluator-dashboard/evaluator-edit-profile/evaluator-edit-profile.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
-import { Router } from '@angular/router';
-
 import { UserService } from '../../core/services/user.service';
-import { User } from 'src/app/core/models/user.model';
-import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import { Position, Office } from '../../core/enums/department.enum'
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { BarangayService } from '../../core/services/barangay.service';
+import { Position, Office } from '../../core/enums/department.enum';
 
 @Component({
   selector: 'app-evaluator-edit-profile',
@@ -16,286 +12,198 @@ import { Position, Office } from '../../core/enums/department.enum'
   styleUrls: ['./evaluator-edit-profile.component.scss']
 })
 export class EvaluatorEditProfileComponent implements OnInit {
-  public photo: string = '';
-  public idFile: FormControl = new FormControl('');
   public selectedFile: File = null;
   public selectedPhoto: File = null;
-  public photoUploaded: boolean = false;
   public maxLength: number = 11;
   public isUpdating: boolean = false;
   public userInfo;
-  public user;
-  public updateUserDetails;
-  public _marital_status:string;
 
-  _birthdateString: string;
+  _barangay: Barangay[];
+  _office:string[] = Object.keys(Office).map(key => Office[key]).filter(k => !(parseInt(k) >= 0));
+  _position:string[] = Object.keys(Position).map(key => Position[key]).filter(k => !(parseInt(k) >= 0));
+  _filteredBarangayOptions: Observable<Barangay[]>;
+  _filteredOfficeOptions: Observable<string[]>;
+  _filteredPositionOptions: Observable<string[]>;
+
   _evaluatorEditProfileForm: FormGroup;
-  _identificationForm: FormGroup;
 
   _displayPhoto: string | ArrayBuffer = '';
   _displayIdPhoto: string | ArrayBuffer = '';
   _submitted = false;
-
-  _filteredOfficeOptions: Observable<string[]>;
-  _filteredPositionOptions: Observable<string[]>;
-
-  _office_options:string[] = Object.keys(Office).map(key => Office[key]).filter(k => !(parseInt(k) >= 0));
-  _position_options:string[] = Object.keys(Position).map(key => Position[key]).filter(k => !(parseInt(k) >= 0));
-
-  private onTouched: () => void;
 
   get displayProfilePhoto(): string | ArrayBuffer {
     return this._displayPhoto ? this._displayPhoto : this.userInfo.photo_path;
   }
 
   get displayIDPhoto(): string | ArrayBuffer {
-    return this._displayIdPhoto
-      ? this._displayIdPhoto
-      : this.userInfo.id_photo_path;
+    return this._displayIdPhoto ? this._displayIdPhoto : this.userInfo.id_photo_path;
   }
-
 
   get evaluatorEditProfileFormControl() {
     return this._evaluatorEditProfileForm.controls;
   }
 
-
   constructor(
     private _fb: FormBuilder,
-    private _router: Router,
-    private _userService: UserService
-
-  ) {
-    this.createForm();
-   }
+    private _userService: UserService,
+    private _barangayService: BarangayService
+  ) {}
 
   createForm() {
     this._evaluatorEditProfileForm = this._fb.group({
-      first_name:['', Validators.required],
-      middle_name:[''],
-      last_name: ['', Validators.required],
-      suffix_name:[''],
-      birthdate:['', Validators.required],
-      gender:['', Validators.required],
-      marital_status:['', Validators.required],
-      home_address: ['', Validators.required ],
-      barangay: ['', Validators.required ],
-
-      employee_no:[''],
-      office:[''],
-      position: [''],
-      contact_number:['', [Validators.required, Validators.maxLength(11),]],
-
-      id_number: ["", Validators.required],
-      id_type: ["", Validators.required],
-
-      
-      
-    },
-    );
+      first_name:[this.userInfo.first_name, Validators.required],
+      middle_name:[this.userInfo.middle_name],
+      last_name:[this.userInfo.last_name, Validators.required],
+      suffix_name:[this.userInfo.suffix_name],
+      birthdate:[this.userInfo.birthdate, Validators.required],
+      marital_status:[this.userInfo.marital_status_id, Validators.required],
+      gender:[this.userInfo.gender, Validators.required],
+      home_address: [this.userInfo.home_address, Validators.required],
+      barangay: [this.userInfo.barangay_id, Validators.required],
+      employee_no:[this.userInfo.employee_detail.employee_no, Validators.required],
+      office:[this.userInfo.employee_detail.office_id, Validators.required],
+      position:[this.userInfo.employee_detail.position, Validators.required],
+      contact_number:[this.userInfo.contact_number, [Validators.required, Validators.maxLength(11),]],
+      id_number:[this.userInfo.id_number, Validators.required],
+      id_type:[this.userInfo.id_type, Validators.required],
+    });
   }
-
-  createUserDetails() {
-    console.log(this.selectedFile);
-    console.log(this.selectedPhoto);
-
-    this.updateUserDetails = {
-      first_name: this._evaluatorEditProfileForm.value.first_name,
-      middle_name: this._evaluatorEditProfileForm.value.middle_name,
-      last_name: this._evaluatorEditProfileForm.value.last_name,
-      suffix_name: this._evaluatorEditProfileForm.value.suffix_name,
-      birthdate: this._evaluatorEditProfileForm.value.birthdate,
-      gender: this._evaluatorEditProfileForm.value.gender,
-      marital_status_id: this._evaluatorEditProfileForm.value.marital_status,
-
-      home_address: this._evaluatorEditProfileForm.value.home_address,
-      //"barangay":this._evaluatorEditProfileForm.value.barangay,
-      contact_number: this._evaluatorEditProfileForm.value.contact_number,
-
-      employee_no: this._evaluatorEditProfileForm.value.employee_no,
-      office: this._evaluatorEditProfileForm.value.office,
-      position: this._evaluatorEditProfileForm.value.position, 
-
-      id_number: this._evaluatorEditProfileForm.value.id_number,
-      id_type: this._evaluatorEditProfileForm.value.id_type,
-    };
-
-    if (this.selectedPhoto) {
-      this.updateUserDetails['photo_path'] = this.selectedPhoto;
-    }
-    if (this.selectedFile) {
-      this.updateUserDetails['id_photo_path'] = this.selectedFile;
-    }
-  }
-
-
 
   onSubmit() {
     this._submitted = true;
-    console.log('date' + this._evaluatorEditProfileForm.value.birthdate);
-    if (this.evaluatorEditProfileFormControl.onTouched) {
-    }
 
     if (this._evaluatorEditProfileForm.valid) {
       this.isUpdating = true;
-      this.createUserDetails();
-      this._userService.setUserInfo(this.updateUserDetails);
+      
+      const user = {
+        first_name: this._evaluatorEditProfileForm.value.first_name,
+        middle_name: this._evaluatorEditProfileForm.value.middle_name,
+        last_name: this._evaluatorEditProfileForm.value.last_name,
+        suffix_name: this._evaluatorEditProfileForm.value.suffix_name,
+        birthdate: this._evaluatorEditProfileForm.value.birthdate,
+        marital_status_id: this._evaluatorEditProfileForm.value.marital_status,
+        gender: this._evaluatorEditProfileForm.value.gender,
+        home_address: this._evaluatorEditProfileForm.value.home_address,
+        barangay: this._evaluatorEditProfileForm.value.barangay_id,
+        employee_no: this._evaluatorEditProfileForm.value.employee_no,
+        office_id: this._evaluatorEditProfileForm.value.office,
+        position: this._evaluatorEditProfileForm.value.position, 
+        contact_number: this._evaluatorEditProfileForm.value.contact_number,
+        id_number: this._evaluatorEditProfileForm.value.id_number,
+        id_type: this._evaluatorEditProfileForm.value.id_type
+      };
+
+      console.log(user);
+      
+      this._userService.setUserInfo(user);
       this._userService
-        .updateUserInfo(this.updateUserDetails, this.userInfo.id)
+        .updateUserInfo(user, this.userInfo.id)
         .subscribe((result) => {
           this.isUpdating = false;
           console.log(result);
-          console.log(this.updateUserDetails);
         });
       this.isUpdating = false;
     }
   }
 
-  _filterOffice(value: string): string[] {
-    const filterValueOffice = value.toLowerCase();
-
-    return this._office_options.filter(_office_option=> _office_option.toLowerCase().includes(filterValueOffice));
-  }
-
-  _filterPosition(value: string): string[] {
-    const filterValuePosition = value.toLowerCase();
-
-    return this._position_options.filter(_position_option=> _position_option.toLowerCase().includes(filterValuePosition));
-  }
-
   openFileChooser() {
-    const element: HTMLElement = document.getElementById(
-      'photo'
-    ) as HTMLElement;
-
+    const element: HTMLElement = document.getElementById('photo') as HTMLElement;
     element.click();
   }
 
   openIdChooser() {
-    const element: HTMLElement = document.getElementById(
-      'idCard'
-    ) as HTMLElement;
-
+    const element: HTMLElement = document.getElementById('id-photo') as HTMLElement;
     element.click();
-  }
-
-  registerOnTouched(fn: any): void {
-    this.onTouched = fn;
-  }
-
-  doBlur() {
-    this.onTouched();
   }
 
   handlePhotoFileChange($event) {
     this.selectedPhoto = $event.target.files[0];
-    this.readSelectedInfo();
+    this.readSelectedPhotoInfo();
   }
 
-  handleFileChangeEvent($event) {
+  handleIDFileChangeEvent($event) {
     this.selectedFile = $event.target.files[0];
     this.readSelectedIdInfo();
   }
 
-  readSelectedInfo() {
+  readSelectedPhotoInfo() {
     if (this.selectedPhoto) {
       let reader = new FileReader();
-
       reader.onload = (res) => {
-        console.log(res);
-
         this._displayPhoto = reader.result;
       };
-
-      reader.readAsDataURL(this.selectedPhoto); // convert to base64 string
+      reader.readAsDataURL(this.selectedPhoto);
     }
   }
 
   readSelectedIdInfo() {
     if (this.selectedFile) {
       let reader = new FileReader();
-
       reader.onload = (res) => {
         this._displayIdPhoto = reader.result;
       };
-
-      reader.readAsDataURL(this.selectedFile); // convert to base64 string
+      reader.readAsDataURL(this.selectedFile);
     }
   }
 
-  getUserInfo() {
-    this._userService.getUserInfo(this.user.uid).subscribe((data) => {
-      this._userService.setUserInfo(data);
-      console.log('ger user' + data);
-    });
+  filterBarangays(value: string): Barangay[] {
+    const filterValue = value.toLowerCase();
+    return this._barangay.filter(option => option.name.toLowerCase().includes(filterValue));
   }
 
-  checkValue(type: string, event: MatDatepickerInputEvent<Date>) {
-    let dd = event.value.getDate();
-    let mm = event.value.getMonth() + 1;
-    let yyyy = event.value.getFullYear();
-    let birthdateString = `${yyyy}-${mm}-${dd}`;
-    this._evaluatorEditProfileForm.value.birthdate = birthdateString;
-    console.log(this._evaluatorEditProfileForm.value.birthdate);
+  filterOffice(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this._office.filter(option=> option.toLowerCase().includes(filterValue));
   }
 
+  filterPosition(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this._position.filter(option=> option.toLowerCase().includes(filterValue));
+  }
 
-
+  displayBarangayName(value: string) {
+    return 'test';
+  }
   
   ngOnInit(): void {
-    
-    this._userService.cast.subscribe(
-      (userSubject) => (this.userInfo = userSubject)
-    
-    );
-
-
-    this.createForm();
-
-    switch(this.userInfo.marital_status_id){
-
-      case 1:
-        this._marital_status = "Single";
-        console.log(this._marital_status);
-        break;
-      case 2:
-        this._marital_status = "Married";
-        console.log(this._marital_status);
-        break;
-      case 3:
-        this._marital_status = "Divorce";
-        console.log(this._marital_status);
-        break;
-      case 4:
-        this._marital_status = "Widowed";
-        console.log(this._marital_status);
-        break;
-
-    }
-
-    this._evaluatorEditProfileForm.patchValue({
-      first_name: this.userInfo.first_name,
-      last_name: this.userInfo.last_name,
-      middle_name: this.userInfo.middle_name,
-
-      suffix_name: this.userInfo.suffix_name,
-      birthdate: this.userInfo.birthdate,
-      gender: this.userInfo.gender,
-      marital_status_id: this._marital_status,
-
-      home_address: this.userInfo.home_address,
-      barangay: this.userInfo.barangay,
-      contact_number: this.userInfo.contact_number,
-
-      
-      id_number: this.userInfo.id_number,
-      id_type: this.userInfo.id_type,
-
+    this._barangayService.getBarangayInfo().subscribe(data => {
+      this._barangay = data;
+      this._filteredBarangayOptions = this.evaluatorEditProfileFormControl.barangay.valueChanges
+      .pipe(
+        startWith(''),
+        map(barangay => barangay ? this.filterBarangays(barangay) : this._barangay.slice())
+      );
     });
 
-    this.user = JSON.parse(localStorage.getItem('user'));
-    this.getUserInfo();
-    console.log(this.userInfo);
+    this._userService.cast.subscribe(data => {
+      if (Object.keys(data).length != 0) {
+        this.userInfo = data;
+        this.userInfo.marital_status_id = String(this.userInfo.marital_status_id);
+        this.createForm();
 
+        this._filteredOfficeOptions = this.evaluatorEditProfileFormControl.office.valueChanges.pipe(
+          startWith(''),
+          map(value => this.filterOffice(value))
+        );
+        
+        this._filteredPositionOptions = this.evaluatorEditProfileFormControl.position.valueChanges.pipe(
+          startWith(''),
+          map(value => this.filterPosition(value))
+        );    
+      }
+    });
   }
+}
+
+export interface Barangay {
+  id: number
+  b_id: number,
+  name:string,
+  locality_id: number;
+  province_id: number; 
+  zip_code: number;
+  region_id: number;
+  country_id: number; 
+  created_at: string,
+  updated_at: string;
 }
--- src/app/evaluator-dashboard/evaluators-summary/evaluators-summary.component.ts
+++ src/app/evaluator-dashboard/evaluators-summary/evaluators-summary.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService} from '../../core/services/auth.service';
 import { LocalStorageService } from '../../core/services/local-storage.service';
 import { RegisterAccountEvaluatorFormService } from '../../core/services/register-account-evaluator-form.service';
@@ -19,19 +18,18 @@ export class EvaluatorsSummaryComponent implements OnInit {
     marital_status_id: this._localStorageService.get('evaluator-registration-marital-status'),
     gender: this._localStorageService.get('evaluator-registration-gender'),
     home_address: this._localStorageService.get('evaluator-registration-home-address'),
-    barangay: this._localStorageService.get('evaluator-registration-barangay'),
+    barangay_id: this._localStorageService.get('evaluator-registration-barangay'),
     employee_no: this._localStorageService.get('evaluator-registration-employee-number'),
     department: this._localStorageService.get('evaluator-registration-office'),
     position: this._localStorageService.get('evaluator-registration-position'),
     contact_number: this._localStorageService.get('evaluator-registration-contact-number'),
-    mobile_no: this._localStorageService.get('evaluator-registration-contact-number'),
     id_type: this._localStorageService.get('evaluator-registration-id-type'),
     id_number: this._localStorageService.get('evaluator-registration-id-number'),
     firebase_uid: this._localStorageService.get('evaluator-registration-firebase-uid'),
     email_address: this._localStorageService.get('evaluator-registration-email-address'),
     emailVerified: this._localStorageService.get('evaluator-registration-email-verified'),
     is_evaluator: this._localStorageService.get('evaluator-registration-is-evaluator'),
-    photo_path: 'test',
+    photo_path: this._localStorageService.get('evaluator-registration-photo'),
     id_photo_path: this._localStorageService.get('evaluator-registration-id')
   }
 
@@ -46,38 +44,54 @@ export class EvaluatorsSummaryComponent implements OnInit {
   }
 
   constructor(
-    private _router: Router,
     private _authService: AuthService,
     private _registerAccountEvaluatorFormService: RegisterAccountEvaluatorFormService,
     private _localStorageService: LocalStorageService
   ) {}
 
   onSubmit(){
+    const photoURL = this.user.photo_path.toString();
+    const photoBlock = photoURL.split(";");
+    const photoContentType = photoBlock[0].split(":")[1];
+    const photoRealData = photoBlock[1].split(",")[1];
+    const photoBlob = this.b64toBlob(photoRealData, photoContentType);
+    
+    this.user.photo_path = new File([photoBlob], 'photo');
+
+    const idURL = this.user.id_photo_path.toString();
+    const idBlock = idURL.split(";");
+    const idContentType = idBlock[0].split(":")[1];
+    const idRealData = idBlock[1].split(",")[1];
+    const idBlob = this.b64toBlob(photoRealData, photoContentType);
+
+    this.user.id_photo_path = new File([idBlob], 'photo');
+
+    console.log(this.user);
+
     this._registerAccountEvaluatorFormService.submitRegisterAccountInfo(this.user).subscribe((res) => {
       this._authService.SendVerificationMail();
       
-    this._localStorageService.remove('evaluator-registration-first-name');
-    this._localStorageService.remove('evaluator-registration-middle-name');
-    this._localStorageService.remove('evaluator-registration-last-name');
-    this._localStorageService.remove('evaluator-registration-suffix-name');
-    this._localStorageService.remove('evaluator-registration-birth-date');
-    this._localStorageService.remove('evaluator-registration-marital-status');
-    this._localStorageService.remove('evaluator-registration-gender');
-    this._localStorageService.remove('evaluator-registration-home-address');
-    this._localStorageService.remove('evaluator-registration-barangay');
-    this._localStorageService.remove('evaluator-registration-employee-number');
-    this._localStorageService.remove('evaluator-registration-office');
-    this._localStorageService.remove('evaluator-registration-position');
-    this._localStorageService.remove('evaluator-registration-contact-number');
-    this._localStorageService.remove('evaluator-registration-contact-number');
-    this._localStorageService.remove('evaluator-registration-id-type');
-    this._localStorageService.remove('evaluator-registration-id-number');
-    this._localStorageService.remove('evaluator-registration-firebase-uid');
-    this._localStorageService.remove('evaluator-registration-email-address');
-    this._localStorageService.remove('evaluator-registration-email-verified');
-    this._localStorageService.remove('evaluator-registration-is-evaluator');
-    this._localStorageService.remove('evaluator-registration-photo');
-    this._localStorageService.remove('evaluator-registration-id')
+      this._localStorageService.remove('evaluator-registration-first-name');
+      this._localStorageService.remove('evaluator-registration-middle-name');
+      this._localStorageService.remove('evaluator-registration-last-name');
+      this._localStorageService.remove('evaluator-registration-suffix-name');
+      this._localStorageService.remove('evaluator-registration-birth-date');
+      this._localStorageService.remove('evaluator-registration-marital-status');
+      this._localStorageService.remove('evaluator-registration-gender');
+      this._localStorageService.remove('evaluator-registration-home-address');
+      this._localStorageService.remove('evaluator-registration-barangay');
+      this._localStorageService.remove('evaluator-registration-employee-number');
+      this._localStorageService.remove('evaluator-registration-office');
+      this._localStorageService.remove('evaluator-registration-position');
+      this._localStorageService.remove('evaluator-registration-contact-number');
+      this._localStorageService.remove('evaluator-registration-id-type');
+      this._localStorageService.remove('evaluator-registration-id-number');
+      this._localStorageService.remove('evaluator-registration-firebase-uid');
+      this._localStorageService.remove('evaluator-registration-email-address');
+      this._localStorageService.remove('evaluator-registration-email-verified');
+      this._localStorageService.remove('evaluator-registration-is-evaluator');
+      this._localStorageService.remove('evaluator-registration-photo');
+      this._localStorageService.remove('evaluator-registration-id');
 
     }, (err => {
       console.log(err);
@@ -93,6 +107,36 @@ export class EvaluatorsSummaryComponent implements OnInit {
       return `${yyyy}-${mm}-${dd}`;
     }
   }
+
+  b64toBlob(b64Data, contentType) {
+    contentType = contentType || '';
+    const sliceSize = 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+  }
+
+  blobToFile(blob, fileName){
+    blob.lastModifiedDate = new Date();
+    blob.name = fileName;
+    return blob;
+}
   
   ngOnInit(): void {}
 }
