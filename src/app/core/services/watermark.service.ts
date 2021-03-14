a3400487fc9bacc048f6c280d0e753f08c28e5af
Thor Remiendo <thorremiendo@gmail.com>
2021-03-14 21:27:48 +0800

    add qr code on office generated forms

--- src/app/core/services/watermark.service.ts
+++ src/app/core/services/watermark.service.ts
@@ -1,35 +1,33 @@
 import { Injectable } from '@angular/core';
 import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
-import { ApiService } from '../services'
+import { ApiService } from '../services';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WaterMarkService {
-  constructor(
-    private api:ApiService,
-  ) {}
+  constructor(private api: ApiService) {}
 
   async insertWaterMark(doc_path: string, doc_type: string) {
     const url = doc_path;
-    const qr_code_path = 'https://s3-ap-southeast-1.amazonaws.com/baguio-ocpas/qr-16157057011.png';
-    const qr_code_bytes = await fetch(qr_code_path).then((res) => res.arrayBuffer());
-  
-  
+    const qr_code_path =
+      'https://s3-ap-southeast-1.amazonaws.com/baguio-ocpas/qr-16157057011.png';
+    const qr_code_bytes = await fetch(qr_code_path).then((res) =>
+      res.arrayBuffer()
+    );
+
     const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
     const pdfDocLoad = await PDFDocument.load(existingPdfBytes);
-    const qr_code = await pdfDocLoad.embedPng(qr_code_bytes)
+    const qr_code = await pdfDocLoad.embedPng(qr_code_bytes);
     const helveticaFont = await pdfDocLoad.embedFont(StandardFonts.Helvetica);
 
     const pages = pdfDocLoad.getPages();
     const pageCount = pages.length;
     const { width, height } = pages[0].getSize();
-    const pngDims = qr_code.scale(0.5)
-    
+    const pngDims = qr_code.scale(0.5);
 
     for (let i = 0; i < pageCount; i++) {
       switch (doc_type) {
-        
         case 'compliant':
           pages[i].drawText('Compliant', {
             x: width / 2 - 150,
@@ -52,12 +50,12 @@ export class WaterMarkService {
             rotate: degrees(-45),
           });
           break;
-          case 'insertQrCode':
-            pages[i].drawImage(qr_code, {
-              x: width / 2 + 150 ,
-              y: height / 2 + 350,
-              width: pngDims.width,
-              height: pngDims.height,
+        case 'insertQrCode':
+          pages[i].drawImage(qr_code, {
+            x: width / 2 + 150,
+            y: height / 2 + 350,
+            width: pngDims.width,
+            height: pngDims.height,
           });
           break;
       }
@@ -65,48 +63,46 @@ export class WaterMarkService {
 
     const pdfBytes = await pdfDocLoad.save();
     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-    const file = window.URL.createObjectURL(blob);
-    window.open(file); // open in new window
+    // const file = window.URL.createObjectURL(blob);
+    // window.open(file); // open in new window
 
     return blob;
   }
 
-  async insertQrCode(doc_path, qr_code_path){
+  async insertQrCode(doc_path, qr_code_path) {
     const url = doc_path;
-    const qr_code_bytes = await fetch(qr_code_path).then((res) => res.arrayBuffer());
+    const qr_code_bytes = await fetch(qr_code_path).then((res) =>
+      res.arrayBuffer()
+    );
 
     const existingPdfBytes = await fetch(url).then((res) => res.arrayBuffer());
     const pdfDocLoad = await PDFDocument.load(existingPdfBytes);
-    const qr_code = await pdfDocLoad.embedPng(qr_code_bytes)
+    const qr_code = await pdfDocLoad.embedPng(qr_code_bytes);
 
     const pages = pdfDocLoad.getPages();
     const pageCount = pages.length;
     const { width, height } = pages[0].getSize();
-    const pngDims = qr_code.scale(0.3)
+    const pngDims = qr_code.scale(0.3);
 
     for (let i = 0; i < pageCount; i++) {
       pages[i].drawImage(qr_code, {
-        x: width / 2 + 200 ,
+        x: width / 2 + 200,
         y: height / 2 + 400,
         width: pngDims.width,
         height: pngDims.height,
-      })
+      });
     }
 
     const pdfBytes = await pdfDocLoad.save();
     const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-    const file = window.URL.createObjectURL(blob);
-    window.open(file); // open in new window
+    // const file = window.URL.createObjectURL(blob);
+    // window.open(file); // open in new window
 
     return blob;
-
-    
   }
 
-  
-
-  generateQrCode(id){
+  generateQrCode(id) {
     const url = `/application/${id}/qrcode`;
-    return this.api.post(url, null)
+    return this.api.post(url, null);
   }
 }
