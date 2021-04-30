6242edab6f81f22899f4de18b6a5c3547087fff4
Thor Remiendo <thorremiendo@gmail.com>
2021-04-30 11:35:30 +0800

    updated downloadable forms

--- src/app/evaluator-dashboard/downloadable-forms/downloadable-forms.component.ts
+++ src/app/evaluator-dashboard/downloadable-forms/downloadable-forms.component.ts
@@ -9,14 +9,15 @@ export class DownloadableFormsComponent implements OnInit {
   public docs = [
     {
       title: 'Certificate of Occupancy Checklist',
-      src: '../../../assets/applicant-checklists/co-CHECKLIST.pdf',
+      src:
+        '../../../assets/applicant-checklists/Checklist_in_Securing_Certificate_of_Occupancy.pdf',
       caption:
         'CHECKLIST IN SECURING Certificate of Occupancy Pursuant to P.D. 1096, National Building Code of the Philippines and its IRR',
     },
     {
       title: 'Fencing Permit Checklist',
       src:
-        '../../../assets/applicant-checklists/CHECKLIST_IN_SECURING_FENCING_PERMIT_(LATEST)_0_(1).pdf',
+        '../../../assets/applicant-checklists/Checklist_in_Securing_Fencing_Permit_(LATEST)_0_(1).pdf',
       caption:
         'CHECKLIST IN SECURING FENCING PERMIT Pursuant to P.D. 1096, National Building Code of the Philippines and its IRR',
     },
@@ -30,13 +31,14 @@ export class DownloadableFormsComponent implements OnInit {
     {
       title: 'Demolition Permit Checklist',
       src:
-        '../../../assets/applicant-checklists/Checklist_in_Securing_Demolition_Permit_(LATEST)_0_(3) (1).pdf',
+        '../../../assets/applicant-checklists/Checklist_in_Securing_Demolition_Permit_(LATEST)_0_(3).pdf',
       caption:
         'CHECKLIST IN SECURING DEMOLITION PERMIT Pursuant to P.D. 1096, National Building Code of the Philippines and it’s IRR',
     },
     {
       title: 'Building Permit Checklist',
-      src: '../../../assets/applicant-checklists/BP-CHECKLIST.pdf',
+      src:
+        '../../../assets/applicant-checklists/Building_Permit_Requirement_Checklist.pdf',
       caption:
         'BUILDING PERMIT REQUIREMENT CHECKLIST Pursuant to P.D. 1096, National Building Code of the Philippines and its IRR ',
     },
