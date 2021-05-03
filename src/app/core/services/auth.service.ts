3a767ba427ced7a9b121806ee3c516a1a7a20885
ANTHONY BASANG <anthony.basang18@gmail.com>
2021-05-03 11:40:45 +0800

    logrocket

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -43,7 +43,7 @@ export class AuthService {
         console.log('this' + user.first_name);
         localStorage.setItem('user', JSON.stringify(user));
         this.isAuthenticatedSubject.next(true);
-        LogRocket.identify('bblmhh/buildpass-staging', {
+        LogRocket.identify('bblmhh/buildpass-production', {
           name: `${user.first_name} ${user.middle_name} ${user.last_name} `,
           email: `${user.email_address}`,
         
