3828db50f396f0ad3d5486af53827b8c89a58833
mr yoso <thorremiendo@gmail.com>
2021-06-13 23:39:36 +0800

    added permit fees dialog

--- src/app/treasury-dashboard/treasury-payment-dialog/treasury-payment-dialog.component.ts
+++ src/app/treasury-dashboard/treasury-payment-dialog/treasury-payment-dialog.component.ts
@@ -1,3 +1,4 @@
+import { TreasuryService } from './../treasury-service.service';
 import { Component, OnInit, Inject } from '@angular/core';
 import {
   FormBuilder,
@@ -10,6 +11,8 @@ import {
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-treasury-payment-dialog',
   templateUrl: './treasury-payment-dialog.component.html',
@@ -23,6 +26,7 @@ export class TreasuryPaymentDialogComponent implements OnInit {
   }
   constructor(
     private fb: FormBuilder,
+    private treasuryService: TreasuryService,
     public dialogRef: MatDialogRef<TreasuryPaymentDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
     public data
@@ -31,6 +35,23 @@ export class TreasuryPaymentDialogComponent implements OnInit {
   ngOnInit(): void {
     this.paymentDetails = this.fb.group({
       receipt: new FormControl('', [Validators.required]),
+    });
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+  handlePaid() {
+    this.isLoading = true;
+    const code = this.data.applicationDetails.ocpas_code;
+    const body = {
+      payment_releasing_status_id: 2,
+      official_receipt_number_releasing: this.paymentDetails.value.receipt,
+    };
+    this.treasuryService.updatePaymentStatus(body, code).subscribe((res) => {
+      Swal.fire('Success!', `Permit Fees - Paid!`, 'success').then((result) => {
+        this.isLoading = false;
+        window.location.reload();
+      });
     });
   }
 }
