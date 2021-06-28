5e7e06a2dd22de773279a029b2d93849745472a5
Evan <nyamet@pm.me>
2021-06-28 21:27:53 +0800

    application timeline

--- src/app/shared/application-timeline/application-timeline.component.ts
+++ src/app/shared/application-timeline/application-timeline.component.ts
@@ -5,6 +5,7 @@ import {
   OnInit,
   Input,
   ViewChild,
+  ElementRef,
 } from '@angular/core';
 import { MatStepper } from '@angular/material/stepper';
 
@@ -24,6 +25,8 @@ export class ApplicationTimelineComponent implements OnInit {
   public applicationTimeline = {};
   public cbaoTimeline = {};
   public cbaoStatus = 'compliant';
+  public cbaoExpandable = false;
+  public timelineLoading = true;
 
   constructor(
     private applicationService: ApplicationInfoService,
@@ -34,7 +37,10 @@ export class ApplicationTimelineComponent implements OnInit {
     if (this.id) this.applicationId = this.id;
     else this.applicationId = this.route.snapshot.paramMap.get('id');
     if (this.type) this.permitType = this.type;
-    this.getApplicationTimeline();
+    this.applicationTimeline = {};
+    this.cbaoTimeline = {};
+    this.cbaoExpandable = false;
+    this.getApplicationTimeline(true);
   }
 
   ngOnChanges(): void {
@@ -42,37 +48,53 @@ export class ApplicationTimelineComponent implements OnInit {
     if (this.type) this.permitType = this.type;
     this.applicationTimeline = {};
     this.cbaoTimeline = {};
-    this.getApplicationTimeline();
+    this.cbaoExpandable = false;
+    this.getApplicationTimeline(false);
   }
 
-  getApplicationTimeline() {
-    this.applicationService
-      .fetchApplicationTmeline(this.applicationId)
-      .subscribe((res) => {
-        res.data.forEach(log => {
-          const officeId = `${log.office_id}.${log.sub_office_id}`;
-          if (!this.applicationTimeline[officeId]) this.applicationTimeline[officeId] = [{...log}];
-          else this.applicationTimeline[officeId].push({...log});
-        });
-
-        this.applicationService
-          .fetchCbaoTimeline(this.applicationId)
-          .subscribe((res) => {
-            this.cbaoStatus = 'compliant';
-            let pendingFlag = false;
-            let nonCompliantFlag = false;
-            res.data.forEach(log => {
-              if (!this.cbaoTimeline[log.name]) this.cbaoTimeline[log.name] = [{...log}];
-              else this.cbaoTimeline[log.name].push({...log});
-            });
-            Object.keys(this.cbaoTimeline).forEach(division => {
-              if (this.cbaoTimeline[division][this.cbaoTimeline[division].length-1].color == 'red') nonCompliantFlag = true;
-              else if (this.cbaoTimeline[division][this.cbaoTimeline[division].length-1].color != 'green') pendingFlag = true;
-            });
-            if (nonCompliantFlag) this.cbaoStatus = 'non-compliant';
-            else if (pendingFlag || !Object.keys(this.cbaoTimeline).length) this.cbaoStatus = 'pending';
-            this.loading = false;
+  getApplicationTimeline(init) {
+    if (init || (!init && !this.timelineLoading)) {
+      this.timelineLoading = true;
+      this.applicationService
+        .fetchApplicationTmeline(this.applicationId)
+        .subscribe((res) => {
+          res.data.forEach(log => {
+            const officeId = `${log.office_id}.${log.sub_office_id}`;
+            if (!this.applicationTimeline[officeId]) this.applicationTimeline[officeId] = [{...log}];
+            else this.applicationTimeline[officeId].push({...log});
           });
-      });
+
+          this.applicationService
+            .fetchCbaoTimeline(this.applicationId)
+            .subscribe((res) => {
+              this.cbaoStatus = 'compliant';
+              let pendingFlag = false;
+              let nonCompliantFlag = false;
+              res.data.forEach(log => {
+                if (!this.cbaoTimeline[log.name]) this.cbaoTimeline[log.name] = [{...log}];
+                else this.cbaoTimeline[log.name].push({...log});
+              });
+              Object.keys(this.cbaoTimeline).forEach(division => {
+                if (this.cbaoTimeline[division].length > 1) this.cbaoExpandable = true;
+                if (this.cbaoTimeline[division][this.cbaoTimeline[division].length-1].color == 'red') nonCompliantFlag = true;
+                else if (this.cbaoTimeline[division][this.cbaoTimeline[division].length-1].color != 'green') pendingFlag = true;
+              });
+              if (nonCompliantFlag) this.cbaoStatus = 'non-compliant';
+              else if (pendingFlag || !Object.keys(this.cbaoTimeline).length) this.cbaoStatus = 'pending';
+              this.loading = false;
+              this.timelineLoading = false;
+            });
+        });
+    }
+  }
+  
+  expandTimelineBox(box) {
+    const boxes = document.querySelectorAll('.application-timeline.flowchart .timeline-box');
+    const target: HTMLElement = document.querySelector(`.application-timeline.flowchart #${box} .timeline-box`);
+    const expanded = target.classList.contains('timeline-full');
+    boxes.forEach(box => {
+      box.classList.remove('timeline-full');
+    });
+    if (!expanded) target.classList.add('timeline-full');
   }
 }
