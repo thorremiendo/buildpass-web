67efcc488fecbb37d779b5157fc89645a78f7787
Evan <nyamet@pm.me>
2021-09-07 04:50:00 +0800

    applications list filtering

--- src/app/shared/applications-list/applications-list.component.ts
+++ src/app/shared/applications-list/applications-list.component.ts
@@ -12,7 +12,7 @@ import { FormControl } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { applicationStatus } from '../../core/enums/application-status.enum';
 import { applicationTypes } from '../../core/enums/application-type.enum';
-import { ApplicationInfoService, EvaluatorService } from '../../core';
+import { ApplicationInfoService } from '../../core';
 import Swal from 'sweetalert2';
 import { constructionType } from '../../core/enums/construction-type.enum';
 @Component({
@@ -30,11 +30,12 @@ export class ApplicationsListComponent implements OnInit, OnChanges {
   public filteredApplications = [];
   public currentItemsToShow = [];
   public loading: boolean = true;
-  public applicationNumber = new FormControl('');
+  public searchKey = new FormControl('');
   public permitType = new FormControl('');
+  public dateStart = new FormControl('');
+  public dateEnd = new FormControl('');
 
   constructor(
-    private evaluatorService: EvaluatorService,
     private applicationService: ApplicationInfoService
   ) {}
 
@@ -45,18 +46,11 @@ export class ApplicationsListComponent implements OnInit, OnChanges {
     this.currentItemsToShow = this.filteredApplications.slice(0 * 5, 0 * 5 + 5);
     this.loading = false;
 
-    this.applicationNumber.valueChanges.subscribe((res) => {
-      this.filteredApplications = this.applications.filter((application) =>
-        application['permit_application_code']
-          .toLowerCase()
-          .includes(res.toLowerCase())
+    this.searchKey.valueChanges.subscribe((res) => {
+      this.filteredApplications = this.applications.filter((application) => 
+        this.searchPerToken(application, res.split(' '))
       );
-      this.currentItemsToShow = this.filteredApplications.slice(
-        this.paginator.pageIndex * this.paginator.pageSize,
-        this.paginator.pageIndex * this.paginator.pageSize +
-          this.paginator.pageSize
-      );
-      this.paginator.firstPage();
+      this.resetPaginator();
     });
 
     this.permitType.valueChanges.subscribe((res) => {
@@ -67,12 +61,21 @@ export class ApplicationsListComponent implements OnInit, OnChanges {
           (application) => application['permit_type_id'] == res
         );
       }
-      this.currentItemsToShow = this.filteredApplications.slice(
-        this.paginator.pageIndex * this.paginator.pageSize,
-        this.paginator.pageIndex * this.paginator.pageSize +
-          this.paginator.pageSize
+      this.resetPaginator();
+    });
+
+    this.dateStart.valueChanges.subscribe((res) => {
+      this.filteredApplications = this.applications.filter((application) => 
+        new Date(application['submitted_at']) > new Date(res)
       );
-      this.paginator.firstPage();
+      this.resetPaginator();
+    });
+
+    this.dateEnd.valueChanges.subscribe((res) => {
+      this.filteredApplications = this.applications.filter((application) => 
+        new Date(application['submitted_at']) < new Date(new Date(res).valueOf() + 1000*3600*24)
+      );
+      this.resetPaginator();
     });
   }
 
@@ -94,6 +97,51 @@ export class ApplicationsListComponent implements OnInit, OnChanges {
       $event.pageIndex * $event.pageSize,
       $event.pageIndex * $event.pageSize + $event.pageSize
     );
+  }
+
+  resetPaginator() {
+    this.currentItemsToShow = this.filteredApplications.slice(
+      this.paginator.pageIndex * this.paginator.pageSize,
+      this.paginator.pageIndex * this.paginator.pageSize +
+        this.paginator.pageSize
+    );
+    this.paginator.firstPage();
+  }
+
+  searchPerToken(application, tokens) {
+    const applicationNumber = application['permit_application_code'];
+    const applicantFullName = `${application['applicant_detail'].first_name} ${application['applicant_detail'].middle_name} ${application['applicant_detail'].last_name} ${application['applicant_detail'].suffix_name}`;
+    let projectLocation = '';
+    if (application['project_detail'].block_number) projectLocation += `BLOCK ${application['project_detail'].block_number} `;
+    if (application['project_detail'].lot_number) projectLocation += `LOT ${application['project_detail'].lot_number} `;
+    if (application['project_detail'].house_number) projectLocation += `#${application['project_detail'].house_number} `;
+    projectLocation += `${application['project_detail'].street_name} ${application['project_detail'].subdivision} ${application['project_detail'].barangay}`;
+
+    let mismatchFlag = false;
+    tokens.every(token => {
+      let matchFlag = false;
+      [applicationNumber, applicantFullName, projectLocation].every(field => {
+        if (this.searchForToken(field, token)) {
+          matchFlag = true;
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      if (!matchFlag) {
+        mismatchFlag = true;
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    return !mismatchFlag;
+  }
+
+  searchForToken(string, key) {
+    return string.toLowerCase().indexOf(key.toLowerCase()) !== -1;
   }
 
   getApplicationStatus(id): string {
@@ -132,15 +180,4 @@ export class ApplicationsListComponent implements OnInit, OnChanges {
       }
     });
   }
-
-  //   btnCategoryClick(status_id: number ){
-
-  //     this.evaluatorService.fetchApplicationByStatus(this.userInfo.id, status_id)
-  //     this.filteredApplications = this.applications.filter(application => application['permit_application_code'].toLowerCase().includes(res.toLowerCase()));
-  //     console.log(this.filteredApplications)
-  //     this.currentItemsToShow = this.filteredApplications.slice(this.paginator.pageIndex*this.paginator.pageSize, this.paginator.pageIndex*this.paginator.pageSize + this.paginator.pageSize);
-  //     console.log(this.currentItemsToShow);
-  //     this.paginator.firstPage();
-
-  // }
 }
