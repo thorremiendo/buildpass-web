9d6c099220be422b09f5076cea0fb9b255f6855c
ANTHONY BASANG <anthony.basang18@gmail.com>
2021-11-16 16:09:47 +0800

    chat ongoing

---
+++ src/app/evaluator-dashboard/evalutor-reminder/evalutor-reminder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evalutor-reminder',
+  templateUrl: './evalutor-reminder.component.html',
+  styleUrls: ['./evalutor-reminder.component.scss']
+})
+export class EvalutorReminderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
