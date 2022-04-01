56276d48b9d124b3ca9b25d01ddfe3253fed592b
Anthony D. Basang <anthonyb@Anthonys-MacBook-Pro.local>
2022-04-01 13:31:44 +0800

    v2.8.7

--- src/staging/environment.mitd.ts
+++ src/staging/environment.mitd.ts
@@ -29,7 +29,7 @@ export const environment = {
     weekend: false,
     receiveApplications: true,
     restrictCbao: false,
-    version: "2.8.6"
+    version: "2.8.7"
   };
   
   
--- src/staging/environment.staging.ts
+++ src/staging/environment.staging.ts
@@ -31,7 +31,7 @@ export const environment = {
     weekend: false,
     receiveApplications: true,
     restrictCbao: false,
-    version: "2.8.6"
+    version: "2.8.7"
   };
   
   
