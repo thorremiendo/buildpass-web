3202d934cadaa593baba6bd5aef572f8dbd28a1e
Thor Remiendo <thorremiendo@gmail.com>
2022-04-25 12:14:51 +0800

    remove duplicate electrical plan

--- src/app/user-dashboard/new-application/occupancy-permit/occupancy-permit.component.ts
+++ src/app/user-dashboard/new-application/occupancy-permit/occupancy-permit.component.ts
@@ -406,7 +406,8 @@ export class OccupancyPermitComponent implements OnInit {
               ) {
                 const oldBpDocs = [
                   125, 177, 212, 4, 117, 199, 195, 140, 29, 115, 30, 63, 61,
-                  167, 64, 65, 216, 50, 43, 26, 59, 62,
+
+                  64, 65, 216, 50, 43, 26, 59, 62,
                 ];
                 //OLD BPS
                 var count = 0;
@@ -437,8 +438,8 @@ export class OccupancyPermitComponent implements OnInit {
                 });
               } else {
                 const releasedBpDocs = [
-                  125, 177, 212, 4, 117, 199, 195, 140, 29, 115, 30, 63, 61,
-                  167, 64, 65, 216, 194, 50, 43, 26, 59, 62,
+                  125, 177, 212, 4, 117, 199, 195, 140, 29, 115, 30, 63, 61, 64,
+                  65, 216, 194, 50, 43, 26, 59, 62,
                 ];
                 let docs = [];
                 let count = 0;
