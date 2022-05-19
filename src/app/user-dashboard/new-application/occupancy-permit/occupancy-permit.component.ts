45cba10f8352b58b8d5c07a10ef43af3f9caa76a
Thor Remiendo <thorremiendo@gmail.com>
2022-05-19 11:10:12 +0800

    added attic, roofdeck, added partial full toggle

--- src/app/user-dashboard/new-application/occupancy-permit/occupancy-permit.component.ts
+++ src/app/user-dashboard/new-application/occupancy-permit/occupancy-permit.component.ts
@@ -405,9 +405,8 @@ export class OccupancyPermitComponent implements OnInit {
                 this.applicationDetails.associated_released_permits.length >= 1
               ) {
                 const oldBpDocs = [
-                  125, 212, 4, 117, 199, 195, 140, 29, 115, 30, 63, 61,
-
-                  64, 65, 216, 50, 43, 26, 59, 62,
+                  125, 212, 4, 117, 199, 195, 140, 29, 115, 30, 63, 61, 64, 65,
+                  216, 50, 43, 26, 59, 62,
                 ];
                 //OLD BPS
                 var count = 0;
