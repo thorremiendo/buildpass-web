2da2343efd93fa9d99256deabe37bce0aeac606c
Thor Remiendo <thorremiendo@gmail.com>
2023-01-20 20:10:25 +0800

    maintenance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  { path: '**', redirectTo: 'error' }, // this needs to be after other routes
-];
+const routes: Routes = [{ path: '**', redirectTo: 'error' }];
 
 @NgModule({
   imports: [
--- src/app/landing/landing-routing.module.ts
+++ src/app/landing/landing-routing.module.ts
@@ -69,6 +69,7 @@ const routes: Routes = [
     path: 'registration',
     component: RegistrationComponent,
   },
+  { path: '**', redirectTo: 'error' },
 ];
 
 @NgModule({
